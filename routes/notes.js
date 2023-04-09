e892a7f716654e18ff97f12d72db2c8b1fad0a35
Abstynent <wookash@Lukaszs-MacBook-Pro.local>
2023-04-09 21:18:50 +0100

    /notes post method added, delete method started to find indexOf note that needs to be removed

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,48 @@
 const notes = require('express').Router();
-const { readFromFile } = require('../helpers/files.js');
+const DB = './db/db.json';
+
+const uuid = require('../helpers/uuid');
+const { readFromFile, readAndAppend } = require('../helpers/files.js');
 
 
 notes.get('/', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+    readFromFile(DB).then((data) => res.json(JSON.parse(data)))
 })
 
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    if(title && text) {
+        let newID = uuid();
+        const newNote = {
+            title,
+            text,
+            id: newID,
+        }
+        
+        const response = {
+            status: 'success',
+            body: newNote,
+        }
+
+        readAndAppend(newNote, DB);
+        console.info(`New note saved, ID: ${newID} 📝`)
+        res.json(response)
+    } else {
+        res.json('Error in saving the note.')
+    }
+})
+
+notes.delete('/:id', (req, res) => {
+    readFromFile(DB).then((data) => {
+        const { id } = req.params;
+
+        let parsedData = JSON.parse(data);
+
+        const indexOfNote = parsedData.findIndex(note => note.id === id );
+        console.log(indexOfNote)
+    })
+
+
+})
 module.exports = notes;
